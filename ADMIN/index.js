6061513834f232d79aaf51d67bca8d7b32e3f06e
unknown <sahribesh0123@gmail.com>
2024-07-28 11:04:40 +0545

    changes

--- ADMIN/index.js
+++ ADMIN/index.js
@@ -5,7 +5,8 @@ const serviceAccount = require('./sadakyatra-firebase.json');
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'sadakyatra-fd6de.appspot.com'
+  storageBucket: 'sadakyatra-fd6de.appspot.com',
+  ignoreUndefinedProperties: true
 });
 
 // Get a Firestore instance
@@ -62,6 +63,11 @@ let alert = require('alert');
 const multer = require('multer');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}));
+
 
 const bucket = admin.storage().bucket();
 
@@ -76,14 +82,20 @@ app.get('/', (req, res) => {
     res.render('index', { message: 'SadakYatra' });
   });
 
+app.post('/add-bus',(req,res)=>{
+
+ console.log(req.body);
+  const name = req.body.name+" sah"
+console.log(name);
+  res.send("form submitted"+req.body);
+})
+
   //to render user-page
 app.get('/users-edit', async(req, res) => {
    console.log('Hello World!');
    try {
     const snapshot = await db.collection('sadakyatra').doc('userDetailsDatabase').collection('users').get();
     const users = snapshot.docs.map(doc => doc.data());
-    // console.log(users);
-    // res.json(users);
     res.render('users-edit', { message:'SadakYatra',data :users });
   } catch (error) {
     // res.status(500).send('Error getting users:', error);
@@ -92,56 +104,183 @@ app.get('/users-edit', async(req, res) => {
   }
   });
   //to render agent-page
-app.get('/agent-edit', (req, res) => {
-   console.log('Hello World!');
-    res.render('agent-edit', { message: 'SadakYatra' });
-  });
-
-  //to render agent-page
-app.get('/news-blog', (req, res) => {
-   console.log('Hello World!');
-    res.render('news-blog', { message: 'SadakYatra' });
-  });
-  //to render bus-edit
-app.get('/bus-edit', (req, res) => {
-   console.log('Hello World!');
-    res.render('bus-edit', { message: 'SadakYatra' });
-  });
-
-  //success page
-app.get('/success', (req, res) => {
-//    console.log('Hello World!');
-    res.render('users-edit', { message: 'Success' });
-  });
-
+  
+  // Function to get all collections inside a document
+  async function getAllCollectionsInDocument(docRef) {
+    const collections = await docRef.listCollections();
+    const collectionsData = {};
+    
+    for (const collection of collections) {
+      const snapshot = await collection.get();
+      const documents = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      collectionsData[collection.id] = documents;
+    }
+    return collectionsData;
+  }
+  //replace hypens
+  function replaceSpacesWithHyphens(inputString) {
+    // Replace all spaces with hyphens
+    return inputString.replace(/\s+/g, '-');
+  }
   //upload-image page
 app.get('/upload-image', (req, res) => {
 //    console.log('Hello World!');
     res.render('upload-image', { message: 'Success' });
   });
-
-  //to update agent data
-  app.post('/agent-update',async (req, res) => {
-    console.log("agent-update", req.body);
-    // await alert("success");
-    console.log('Form data received successfully');
-    res.render('agent-edit',{ message: 'SadakYatra' });
+  //to render agent-page
+  app.get('/news-blog',async (req, res) => {
+    console.log('Hello World!');
+    try {
+      const resDef = db.collection('sadakyatra').doc('packages');
+      const collectionsData = await getAllCollectionsInDocument(resDef);
+      
+      res.render('news-blog', { message:'SadakYatra',data :collectionsData });
+    } catch (error) {
+      res.status(500).send('Error getting users');
+    }
+  });
+  
+  //reservation details
+  app.get('/reservation-edit', async(req, res) => {
+     console.log('Hello World!');
+     try {
+      const resDef = db.collection('sadakyatra').doc('reservation');
+      const collectionsData = await getAllCollectionsInDocument(resDef);
+      
+      res.render('reservation-edit', { message:'SadakYatra',data :collectionsData });
+    } catch (error) {
+      res.status(500).send('Error getting users');
+    }
     });
 
-  //to update and add news-blog data
-  app.post('/news-update',async (req, res) => {
-    console.log("news-blog", req.body);
-    // await alert("success");
-    console.log('Form data received successfully');
-    res.render('news-blog',{ message: 'SadakYatra' });
+    //to reservation update
+    app.post('/reservation-update',upload.none(),async (req, res) => {
+      console.log("reservation-update", req.body);
+      const { uid , location ,status} = req.body;
+    console.log(uid);
+    console.log(location);
+    console.log(status);
+      // await alert("success");
+    try{
+      await db.collection('sadakyatra').doc('reservation').collection(location).doc(uid).update(
+        {
+          status :status
+        }
+      ); 
+      res.status(200).send('success');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('failed');
+    }
+      console.log('Form data received successfully');
+      
     });
-  //to update and add news-blog data
-  app.post('/bus-edit',async (req, res) => {
-    console.log("news-blog", req.body);
-    // await alert("success");
-    console.log('Form data received successfully');
-    res.render('bus-edit',{ message: 'SadakYatra' });
+
+  //to delete reservation
+app.post('/delete-reservation',upload.none(), async (req, res) => {
+  try {
+    const { uid , location} = req.body;
+    console.log(uid);
+    console.log(location);
+    await db.collection('sadakyatra').doc('reservation').collection(location).doc(uid).delete(); 
+    res.status(200).json('success');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('failed');
+  }
+});
+  
+  //to render bus-edit
+app.get('/bus-edit', async(req, res) => {
+   console.log('Hello World!');
+    try {
+      const resDef = db.collection('sadakyatra').doc('busTicketDetails');
+      const collectionsData = await getAllCollectionsInDocument(resDef);
+      
+      // console.log(collectionsData);
+
+      res.render('bus-edit', { message:'SadakYatra',data :collectionsData });
+    } catch (error) {
+      res.status(500).send('Error getting users');
+      // var message = "Error Getting Users!"
+      // alert(message);
+    }
+  });
+
+
+
+  //to create news and package data
+ app.post('/package-create',upload.single('image'),async (req, res) => {
+  //  alert("success");
+  try {
+    const name = req.body.name;
+    const price = req.body.price;
+    const package = req.body.package;
+   
+    const imageFile = req.file;
+    const folderName = "tour_packages_images";
+
+    if (!imageFile) {
+      return res.status(400).send('Image file and description are required.');
+    }
+    const resultArray = imageFile.originalname.split('.');
+    const imageName = resultArray[0];
+    const modifiedName = replaceSpacesWithHyphens(imageName);
+    const modifiedUserImage = (name+'-'+ modifiedName);
+    const imageName1 = `${folderName}/${modifiedUserImage}`;
+    const imageBlob = bucket.file(imageName1);
+    
+    await imageBlob.save(imageFile.buffer, {
+      contentType: imageFile.mimetype,
+      metadata: { firebaseStorageDownloadTokens: uuidv4() }
     });
+
+    console.log("\n\nimage file -----------------------------\n\ninside create---user");
+
+    const file = bucket.file(imageName1); 
+    const [url] = await file.getSignedUrl({
+      action: 'read',
+      expires: '03-01-2500'
+    });
+    console.log(url);
+
+    //generate uid 
+    const userDocRef = db.collection('sadakyatra').doc('packages').collection(package).doc();
+    const userId = userDocRef.id;
+    console.log(userId);
+    // Store description and image URL in Firestore
+    await userDocRef.set({
+      uid: userId,
+      name: name,
+      price: price,
+      location:package,
+      imageUrl: url,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+
+    console.log(modifiedName);
+
+  res.status(200).send('Files uploaded successfully');
+  } catch (error) {
+    console.error('Error uploading files:', error);
+    res.status(500).send('Error uploading files');
+  }
+
+});
+  
+//to delete package
+app.post('/delete-package',upload.none(), async (req, res) => {
+  try {
+    const { uid , package} = req.body;
+    console.log(uid);
+    console.log(package);
+    await db.collection('sadakyatra').doc('packages').collection(package).doc(uid).delete();
+    res.status(200).json('success');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('failed');
+  }
+});
 
 // Sample data
 const data = [
@@ -167,11 +306,6 @@ app.get('/users', async (req, res) => {
   }
 });
 
-//replace hypens
-function replaceSpacesWithHyphens(inputString) {
-  // Replace all spaces with hyphens
-  return inputString.replace(/\s+/g, '-');
-}
 //upload image and description 
 app.post('/upload', upload.single('image'), async (req, res) => {
   try {
@@ -233,15 +367,67 @@ app.get('/image-url', async (req, res) => {
   }
 });
 
+  //to update bus data
+app.post('/bus-create',upload.none(),async (req, res) => {
+    console.log("bus-create");
+    // await alert("success");
+  try{
+    const { busName, busNumber, location, price, busType } = req.body;
+    console.log('Bus Name:', busName);
+    console.log('Bus Number:', busNumber);
+    console.log('Location:', location);
+    console.log('Price:', price);
+    console.log('Bus Type:', busType);
+    console.log(req.body);
+ 
+    //generate uid 
+    const userDocRef = db.collection('sadakyatra').doc('busTicketDetails').collection(location).doc();
+    const userId = userDocRef.id;
+    console.log(userId);
+    // Store description and image URL in Firestore
+    await userDocRef.set({
+      uid: userId,
+      busName: busName,
+      busNumber: busNumber,
+      location: location,
+      busType: busType,
+      price: price,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+
+    console.log(userId);
+    res.status(200).json('success');
+    }catch(e){
+      console.log(e);
+      res.status(500).json('failed');
+    }
+   
+  });
+
+//to delete bus
+app.post('/delete-bus',upload.none(), async (req, res) => {
+  try {
+    const { uid , location} = req.body;
+    console.log(uid);
+    console.log(location);
+    await db.collection('sadakyatra').doc('busTicketDetails').collection(location).doc(uid).delete();
+    res.status(200).json('success');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('failed');
+  }
+
+});
+
  //to create user data
- app.post('/user-create',upload.single('image'),async (req, res) => {
+app.post('/user-create',upload.single('image'),async (req, res) => {
   //  alert("success");
   try {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
     const contact = req.body.contact;
-    const userId = uuidv4(); 
+   
 
     // const description = req.body.description;
     const imageFile = req.file;
@@ -270,8 +456,13 @@ app.get('/image-url', async (req, res) => {
       expires: '03-01-2500'
     });
     console.log(url);
+
+    //generate uid 
+    const userDocRef = db.collection('sadakyatra').doc('userDetailsDatabase').collection('users').doc();
+    const userId = userDocRef.id;
+    console.log(userId);
     // Store description and image URL in Firestore
-    await db.collection('sadakyatra').doc('userDetailsDatabase').collection('users').doc(userId).add({
+    await userDocRef.set({
       uid: userId,
       username: username,
       email: email,
@@ -281,7 +472,6 @@ app.get('/image-url', async (req, res) => {
       createdAt: admin.firestore.FieldValue.serverTimestamp()
     });
 
-    // console.log('Description:', description);
     console.log(modifiedName);
 
   res.status(200).send('Files uploaded successfully');
@@ -365,13 +555,15 @@ app.delete('/delete-user/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
     console.log(userId);
-    await db.collection('users').doc(userId).delete();
+    await db.collection('sadakyatra').doc('userDetailsDatabase').collection('users').doc(userId).delete();
     res.json({ success: true, message: 'User deleted successfully' });
   } catch (error) {
     console.error('Error deleting user:', error);
     res.status(500).send('Error deleting user');
   }
 });
+
+
 
 app.listen(port, () => { 
     // getDocument();
